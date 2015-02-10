56ecabff8c3b0462823028c2b1294c41a67a77bc
zhuangqi <zhuangqi@zhuangqideMacBook-Air.local>
2015-02-10 16:29:26 +0800

    Initial Commit

---
+++ leanSwift1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  leanSwift1
+//
+//  Created by zhuangqi on 15/2/10.
+//  Copyright (c) 2015年 zhuangqi. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
